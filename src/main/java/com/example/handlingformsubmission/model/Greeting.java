bc9d8544f8ea0eacd8b07b9b1a9e6b071b8a8e84
ittimfn <ittimfn+github@gmail.com>
2022-07-18 17:48:51 +0900

    lombok使用、package変更

---
+++ src/main/java/com/example/handlingformsubmission/model/Greeting.java
@@ -0,0 +1,13 @@
+package com.example.handlingformsubmission.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Greeting {
+
+  private long id;
+  private String content;
+
+}
